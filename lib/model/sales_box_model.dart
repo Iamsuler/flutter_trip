a05c5c30d07aba425ce64639b85d5fb41c113efc
suler <heweifang0612@163.com>
2021-07-10 23:02:12 +0800

    修改SalesBoxModel模型

--- lib/model/sales_box_model.dart
+++ lib/model/sales_box_model.dart
@@ -3,43 +3,33 @@ import 'package:flutter_trip/model/common_model.dart';
 class SalesBoxModel {
   final String icon;
   final String moreUrl;
-  final CommonModel bigCard1;
-  final CommonModel bigCard2;
-  final CommonModel smallCard1;
-  final CommonModel smallCard2;
-  final CommonModel smallCard3;
-  final CommonModel smallCard4;
+  final List<CommonModel> list;
 
   SalesBoxModel(
       {required this.icon,
       required this.moreUrl,
-      required this.bigCard1,
-      required this.bigCard2,
-      required this.smallCard1,
-      required this.smallCard2,
-      required this.smallCard3,
-      required this.smallCard4});
+      required this.list});
 
   factory SalesBoxModel.fromJson(Map<String, dynamic> json) {
+    List<CommonModel> list = [];
+    json.forEach((key, value) {
+      if(value is Map ) {
+        if(key.contains('bigCard')) {
+          list.insert(0, CommonModel.fromJson((value as Map<String, dynamic>)));
+        } else {
+          list.add(CommonModel.fromJson(value as Map<String, dynamic>));
+        }
+      }
+    });
     return SalesBoxModel(
         icon: json['icon'],
         moreUrl: json['moreUrl'],
-        bigCard1: CommonModel.fromJson(json['bigCard1']),
-        bigCard2: CommonModel.fromJson(json['bigCard2']),
-        smallCard1: CommonModel.fromJson(json['smallCard1']),
-        smallCard2: CommonModel.fromJson(json['smallCard2']),
-        smallCard3: CommonModel.fromJson(json['smallCard3']),
-        smallCard4: CommonModel.fromJson(json['smallCard4']));
+        list: list);
   }
 
   Map<String, dynamic> toJson() => <String, dynamic>{
     'icon': icon,
     'moreUrl': moreUrl,
-    'bigCard1': bigCard1.toJson(),
-    'bigCard2': bigCard2.toJson(),
-    'smallCard1': smallCard1.toJson(),
-    'smallCard2': smallCard2.toJson(),
-    'smallCard3': smallCard3.toJson(),
-    'smallCard4': smallCard4.toJson(),
+    'list': list.map((e) => e.toJson()).toList()
   };
 }
